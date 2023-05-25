c6e0fde0521d04ff65d583f07982c319a9568595
Ailen Goitia <ailugoitia96@gmail.com>
2023-05-25 20:36:24 -0300

    SegundaEntrega+Goitia

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element = {<ItemListContainer/>} />
         <Route path="/item/:id" element = {<ItemDetailContainer/>}/>
         <Route path="/productos" element = {<ItemListContainer/>}/>
-        <Route path="/productos/categoria" element = {<ItemListContainer/>}/>
+        <Route path="/productos/:categoria" element = {<ItemListContainer/>}/>
         <Route path="/" element = {<ItemListContainer/>} />
       </Routes>
 
